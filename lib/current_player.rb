ab33195412b88ab1eb6b1e580b52bf71200467fc
Dominic Chapman <dominicchapman@me.com>
2017-09-26 00:00:41 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,13 +10,12 @@ def turn_count(board)
 end
 
 # first player assumed to be X, so odd moves i.e. 1, 3, 5, 7, 9
-# 
 def current_player(board)
 
   if turn_count(board) % 2
-    return "X"
-  else
     return "O"
+  else
+    return "X"
   end
 
 end
