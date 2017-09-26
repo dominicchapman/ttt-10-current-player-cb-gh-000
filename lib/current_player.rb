8071bb0fd3f5f5666b6939ff0ba3472d2cab1bd1
Dominic Chapman <dominicchapman@me.com>
2017-09-26 00:03:55 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,12 +6,14 @@ def turn_count(board)
       move_count += 1
     end
   end
+  # move_count = 5
   move_count
 end
 
 # first player assumed to be X, so odd moves i.e. 1, 3, 5, 7, 9
 def current_player(board)
 
-  turn_count(board) % 2 == 0 ? "X" : "O"
+  # if_this_is_a_true_value ? then_the_result_is_this : else_it_is_this
+  turn_count(board) % 2 == 0 ? "O" : "X"
 
 end
